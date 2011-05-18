18fccc7f7ae266eeca1c82a2e511b535c6896211
Wally Glutton <stungeye@gmail.com>
2011-05-18 13:24:19 -0500

    Completed level 4

--- stungeye-beginner/player.rb
+++ stungeye-beginner/player.rb
@@ -2,10 +2,11 @@ class Player
   def play_turn(warrior)
     if warrior.feel.enemy?
       warrior.attack!
-    elsif warrior.health < 10
+    elsif warrior.health < 19 && warrior.health >= @health
       warrior.rest!
     else
       warrior.walk!
     end
+    @health = warrior.health
   end
 end
