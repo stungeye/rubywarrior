80e158ca51c89827b2a0dcbb9158d64eb9418e09
Wally Glutton <stungeye@gmail.com>
2011-05-18 13:51:24 -0500

    Completed level six

--- stungeye-beginner/player.rb
+++ stungeye-beginner/player.rb
@@ -1,20 +1,42 @@
 class Player
   def play_turn(warrior)
-    felt = warrior.feel
-    if felt.captive?
-      warrior.rescue!
-    elsif felt.enemy?
-      warrior.attack!
+    if should_retreat?(warrior, @health)
+      @direction = :backward 
+    end
+    space = warrior.feel(direction)
+    if space.wall?
+      toggle_direction!
+      space = warrior.feel(direction)
+    end
+    if space.captive?
+      warrior.rescue!(direction)
+    elsif space.enemy?
+      warrior.attack!(direction)
     elsif should_rest?(warrior, @health)      
       warrior.rest!
     else
-      warrior.walk!
+      warrior.walk!(direction)
     end
     @health = warrior.health
   end
 
+  def direction
+    @direction ||= :backward
+  end
+
+  def toggle_direction!
+    if @direction == :forward
+      @direction = :backward
+    else
+      @direction = :forward
+    end
+  end
 
   def should_rest?(warrior, past_health)
     warrior.health < 19 && warrior.health >= past_health
+  end
+
+  def should_retreat?(warrior, past_health)
+    warrior.health < 9 && warrior.health < past_health
   end
 end
