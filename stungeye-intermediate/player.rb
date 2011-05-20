74e294a93c0901f4689f252b9be0bd1aa34a3b16
Wally Glutton <stungeye@gmail.com>
2011-05-20 17:49:35 -0500

    Completed level 3

--- stungeye-intermediate/player.rb
+++ stungeye-intermediate/player.rb
@@ -1,11 +1,30 @@
 class Player
+  DIRECTIONS = [:forward, :right, :backward, :left]
+
   def play_turn(warrior)
-    direction_sought = warrior.direction_of_stairs
-    space = warrior.feel direction_sought
-    if space.enemy?
-      warrior.attack! direction_sought
+    d = warrior.direction_of_stairs
+    around = surroundings(warrior)
+    if around[:enemies].count > 2
+      warrior.bind! around[:enemies].first
+    elsif around[:spaces][d].enemy?
+      warrior.attack! d
     else
-      warrior.walk! warrior.direction_of_stairs
+      warrior.walk! d
     end
   end
+
+  def surroundings(warrior)
+    spaces = {}
+    enemies = []
+    DIRECTIONS.map do |d|
+      spaces[d] = warrior.feel(d)
+      enemies << d  if spaces[d].enemy?
+    end
+    { 
+      :spaces => spaces,
+      :enemies => enemies 
+    }
+  end
+  
 end
+
