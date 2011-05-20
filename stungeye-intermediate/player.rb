da21964b9dc5deb48b0b0f635299e7b11e49295e
Wally Glutton <stungeye@gmail.com>
2011-05-20 16:47:16 -0500

    Level 2 completed.

--- stungeye-intermediate/player.rb
+++ stungeye-intermediate/player.rb
@@ -1,5 +1,11 @@
 class Player
   def play_turn(warrior)
-    warrior.walk! warrior.direction_of_stairs
+    direction_sought = warrior.direction_of_stairs
+    space = warrior.feel direction_sought
+    if space.enemy?
+      warrior.attack! direction_sought
+    else
+      warrior.walk! warrior.direction_of_stairs
+    end
   end
 end
