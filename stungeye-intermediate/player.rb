60bfd8d2c75c431dbbe13da8cf8308ef6c824afc
Wally Glutton <stungeye@gmail.com>
2011-05-21 11:39:26 -0500

    Completes level 4, but harse on captives.

--- stungeye-intermediate/player.rb
+++ stungeye-intermediate/player.rb
@@ -1,29 +1,69 @@
 class Player
   DIRECTIONS = [:forward, :right, :backward, :left]
+  REVERSE_DIRECTION = { :forward  => :backward,
+                        :backward => :forward,
+                        :left     => :right,
+                        :right    => :left }
 
-  def play_turn(warrior)
-    d = warrior.direction_of_stairs
-    around = surroundings(warrior)
-    if around[:enemies].count > 2
-      warrior.bind! around[:enemies].first
-    elsif around[:spaces][d].enemy?
-      warrior.attack! d
-    else
-      warrior.walk! d
-    end
+  def initialize 
+    @previous_steps = []
+    @memory = {}
+    @health = 20
   end
 
-  def surroundings(warrior)
+
+  def play_turn(warrior)
+    @warrior = warrior
+    spaces, enemies, captives = close_by(warrior)
+
+    if should_retreat?
+      walk! retreat_direction
+    elsif enemies.count > 1
+      bind! enemies.first
+    elsif enemies.count > 0
+      attack! enemies.first
+    elsif captives.count > 0
+      attack! captives.first
+    elsif should_rest?      
+      rest!
+    else 
+      walk! direction_of_stairs
+      @previous_steps << direction_of_stairs
+    end
+
+    remember_health
+  end
+
+  def retreat_direction
+    REVERSE_DIRECTION[@previous_steps.pop]
+  end
+
+  def remember_health
+    @health = health
+  end
+
+  def should_rest?
+    health < 19 && health >= @health
+  end
+
+  def should_retreat?
+    health < 10 && health < @health
+  end
+
+  def close_by(warrior)
     spaces = {}
     enemies = []
+    captives = []
     DIRECTIONS.map do |d|
       spaces[d] = warrior.feel(d)
       enemies << d  if spaces[d].enemy?
+      captives << d  if spaces[d].captive?
     end
-    { 
-      :spaces => spaces,
-      :enemies => enemies 
-    }
+    [spaces, enemies, captives]
+  end
+
+  def method_missing(m, *args)
+    @warrior.send(m, *args)
   end
   
 end
