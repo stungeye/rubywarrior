07be8d4dbda4d8fd375c34626fd3a14a7dedca64
Wally Glutton <stungeye@gmail.com>
2011-05-21 23:49:48 -0500

    Completes level 5, but needs refactoring.

--- stungeye-intermediate/player.rb
+++ stungeye-intermediate/player.rb
@@ -15,25 +15,31 @@ class Player
     @warrior = warrior
 
     all_enemies, all_captives = listen_and_identify
-    enemies, captives = feel_and_identify
+    enemies, captives, empties = feel_and_identify
+
+puts "Empties:"
+puts empties
 
     if should_retreat?(enemies)
       walk! retreat_direction
     elsif enemies.count > 1
-      bind! direction_of enemies.first
+      bind! direction_of(enemies.first)
     elsif enemies.count > 0
-      attack! direction_of enemies.first
+      attack! direction_of(enemies.first)
     elsif should_rest?      
       rest!
     elsif captives.count > 0
-      rescue! direction_of captives.first
+      rescue! direction_of(captives.first)
     else 
       if all_captives.count > 0
-        d = direction_of all_captives.first
+        d = direction_of(all_captives.first)
       elsif all_enemies.count > 0
-        d = direction_of all_enemies.first
+        d = direction_of(all_enemies.first)
       else
         d = direction_of_stairs
+      end
+      if feel(d).stairs? && (all_captives.count > 0 || all_enemies.count > 0)
+        d = direction_of(empties.first)
       end
       walk! d
       @previous_steps << d
@@ -73,11 +79,13 @@ class Player
   def identify(spaces)
     enemies = []
     captives = []
+    empties = []
     spaces.each do |space|
       enemies << space  if space.enemy?
       captives << space if space.captive?
+      empties << space  if space.empty? && !space.stairs?
     end
-    [enemies, captives]
+    [enemies, captives, empties]
   end
 
   def method_missing(m, *args)
